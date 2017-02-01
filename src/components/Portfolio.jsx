f90957dedd351f1c592a7709552de76e334b893c
Matt Lewis <domattthings@gmail.com>
2017-02-01 18:42:01 +0000

    Fixed navigation to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,38 +11,40 @@ const carouselConfig = {
 };
 
 const Portfolio = () => (
-  <Carousel {...carouselConfig}>
-    <PortfolioItem title="Calculator-Redux"
-                   link="https://github.com/mattmatters/calculator-redux"
-                   ghLink="https://github.com/mattmatters/calculator-redux"
-                   img="calc"
-                   description="Chainable calculator build in React-Redux"
-    />
-    <PortfolioItem title="Barlen Electronics"
-                   link="http://barlenelectronics.com"
-                   ghLink="https://github.com/mattmatters/lennyWebsite"
-                   img="grandma"
-                   description="Website design and DNS management"
-    />
-    <PortfolioItem title="Leaderboard-Redux"
-                   link="https://github.com/mattmatters/calculator-redux"
-                   ghLink="https://github.com/mattmatters/leaderboard-redux"
-                   img="grandma"
-                   description="React-Redux Experiment with asynchronous operations"
-    />
-    <PortfolioItem title="Lou Reed Tribute"
-                   link="http://loureedtribute.com.s3-website-us-east-1.amazonaws.com/"
-                   ghLink="https://github.com/mattmatters/louReedTributeSite"
-                   img="grandma"
-                   description="Experiment in creating a website utilizing only Bootstrap as a stylesheet"
-    />
-    <PortfolioItem title="Markdown Preview"
-                   link="https://github.com/mattmatters/calculator-redux"
-                   ghLink="https://github.com/mattmatters/markdownPreview"
-                   img="grandma"
-                   description="Auto-updating Markdown to HTML converter"
-    />
-  </Carousel>
+  <div id="portfolio">
+    <Carousel {...carouselConfig}>
+      <PortfolioItem title="Calculator-Redux"
+                     link="https://github.com/mattmatters/calculator-redux"
+                     ghLink="https://github.com/mattmatters/calculator-redux"
+                     img="calc"
+                     description="Chainable calculator build in React-Redux"
+      />
+      <PortfolioItem title="Barlen Electronics"
+                     link="http://barlenelectronics.com"
+                     ghLink="https://github.com/mattmatters/lennyWebsite"
+                     img="grandma"
+                     description="Website design and DNS management"
+      />
+      <PortfolioItem title="Leaderboard-Redux"
+                     link="https://github.com/mattmatters/calculator-redux"
+                     ghLink="https://github.com/mattmatters/leaderboard-redux"
+                     img="grandma"
+                     description="React-Redux Experiment with asynchronous operations"
+      />
+      <PortfolioItem title="Lou Reed Tribute"
+                     link="http://loureedtribute.com.s3-website-us-east-1.amazonaws.com/"
+                     ghLink="https://github.com/mattmatters/louReedTributeSite"
+                     img="grandma"
+                     description="Experiment in creating a website utilizing only Bootstrap as a stylesheet"
+      />
+      <PortfolioItem title="Markdown Preview"
+                     link="https://github.com/mattmatters/calculator-redux"
+                     ghLink="https://github.com/mattmatters/markdownPreview"
+                     img="grandma"
+                     description="Auto-updating Markdown to HTML converter"
+      />
+    </Carousel>
+  </div>
 );
 
 export default Portfolio;
