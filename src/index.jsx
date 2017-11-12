052fd6bc2dce9b1835d84814c61c2a913832bc4e
Matt Lewis <mlewis@c3metrics.com>
2017-11-11 19:52:15 -0500

    Removed Redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import createLogger from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
 import App from './components/App';
-import reducers from './reducers/index';
 import '../style/style.scss';
 
-const loggerMiddleware = createLogger();
-
-const store = createStore(
-  reducers,
-  applyMiddleware(
-    thunkMiddleware,
-    loggerMiddleware,
-  ),
-);
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root'),
 );
